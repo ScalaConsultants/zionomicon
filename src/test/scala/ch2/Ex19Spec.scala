c9f03b8e011e86615472bcb3ace9c94509b089c4
juancho <juanchoj1024@gmail.com>
2022-02-23 17:33:07 -0500

    commonality renamed

--- src/test/scala/ch2/Ex19Spec.scala
+++ src/test/scala/ch2/Ex19Spec.scala
@@ -12,14 +12,14 @@ object Ex19Spec extends DefaultRunnableSpec:
   def spec: Spec[TestConsole, TestFailure[IOException], TestSuccess] =
     suite("Ex19Spec")(
       test("readUntil keeps reading input until it gets the right one that it, in turn, will return")(
-        commonTest("wrong", "incorrect", "not even close", CorrectInput)
+        readUntilTest("wrong", "incorrect", "not even close", CorrectInput)
       ),
       test("do not read after it gets the right one")(
-        commonTest(CorrectInput, "don't")
+        readUntilTest(CorrectInput, "don't")
       ),
     )
 
-  private def commonTest(ins: String*) =
+  private def readUntilTest(ins: String*) =
     for
       _  <- TestConsole.feedLines(ins*)
       res <- readUntil(_ == CorrectInput)
