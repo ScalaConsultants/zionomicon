59b4a590bef1c5c4a0bf28a79f312725f4579306
juancho <juanchoj1024@gmail.com>
2022-02-23 16:58:34 -0500

    added extra-input test case

--- src/test/scala/ch2/Ex19Spec.scala
+++ src/test/scala/ch2/Ex19Spec.scala
@@ -7,13 +7,21 @@ import Ex19.*
 import java.io.IOException
 
 object Ex19Spec extends DefaultRunnableSpec:
-  def spec: Spec[TestConsole, TestFailure[IOException], TestSuccess] = suite("Ex19Spec")(
-    test("readUntil keeps reading input until it gets the right one that it, in turn, will return") {
-      val correctInput = "-999"
+  def spec: Spec[TestConsole, TestFailure[IOException], TestSuccess] =
+    val correctInput = "777"
+    val f = readUntil(_ == correctInput)
 
-      for
-        _  <- TestConsole.feedLines("wrong", "incorrect", "not even close", correctInput)
-        res <- readUntil(_ == correctInput)
-      yield assert(res)(equalTo(correctInput))
-    }
-  )
+    suite("Ex19Spec")(
+      test("readUntil keeps reading input until it gets the right one that it, in turn, will return") {
+        for
+          _  <- TestConsole.feedLines("wrong", "incorrect", "not even close", correctInput)
+          res <- f
+        yield assert(res)(equalTo(correctInput))
+      },
+      test("do not read after it gets the right one") {
+        for
+          _  <- TestConsole.feedLines(correctInput, "don't")
+          res <- f
+        yield assert(res)(equalTo(correctInput))
+      }
+    )
